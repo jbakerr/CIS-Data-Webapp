86af633abc0ce1a239f0ecff7bc2848a8793b214
Baker Renneckar <bakerrenneckar@gmail.com>
2018-11-06 15:08:34 -0800

    Upaded metric import to allow for both math 1 and 2

--- scripts/progress_monitoring.r
+++ scripts/progress_monitoring.r
@@ -10,7 +10,7 @@ progress_import_script <- function(progress){
   colnames(progress) <- make.names(colnames(progress))
   
 # Setting Vectors that will be used throughout program
-  metrics <- c("Math","Science","ELA", "Suspensions", "Attendance_Rate")
+  metrics <- c("Math", "Math2", "Science","ELA", "Suspensions", "Attendance_Rate")
   
   elem <- c(
     "Glenn Elementary School", "Eno Valley Elementary",
@@ -30,6 +30,7 @@ progress_import_script <- function(progress){
 # exactly as found in the excel file. 
   progress$Metric[progress$Metric =='Core Course Grades: Eng/Lang Arts/Reading/Writing'] <- "ELA"
   progress$Metric[progress$Metric =='Core Course Grades: Math 1'] <- "Math"
+  progress$Metric[progress$Metric =='Core Course Grades: Math 2'] <- "Math2"
   progress$Metric[progress$Metric =='Core Course Grades: Science'] <- "Science"
   progress <- progress[ ! (progress$Metric =='Standardized test score: English / Language Arts'),]
   progress <- progress[ ! (progress$Metric =='Standardized test score: Science'),]
@@ -37,8 +38,8 @@ progress_import_script <- function(progress){
   
   
 # Rearranged column orders
-  progress <- progress[,c(1:8, 9:12,21,13, 14:20)]
-  
+  # progress <- progress[,c(1:14, 9:12,21,13, 14:20)]
+  # c(1:8, 9:12,21,13, 14:20)
 
 # Changing Student Name to Student
   colnames(progress)[4] <- c("Student")
@@ -51,6 +52,9 @@ progress_import_script <- function(progress){
   progress$School.Year <- NULL
   progress$Baseline.Date <- NULL
   progress$Goal <- NULL
+  progress$Enrollment.Begin.Date <- NULL
+  progress$Enrollment.Status <- NULL
+  progress$EnrollmentID <- NULL
 
 # removing unwanted metrics - if the column title doesn't apear in the list of 
 # metrics from the environment.r script, then they will be deleted.
@@ -68,7 +72,7 @@ progress_import_script <- function(progress){
 
   progress <- progress[!duplicated(progress[,c(
     "School", "Student", "Metric", "Value", "Student.ID",
-    "Period", "Enrollment.Status"
+    "Period"
     )]), ]
 
 #Creating a wide data frame
@@ -102,6 +106,11 @@ progress_import_script <- function(progress){
                                       TRUE, FALSE
                                       )
   
+  progress$math_error <- ifelse(progress$math_error == TRUE & (is.na(progress$Baseline_Math2) & 
+                                  is.na(progress$Target_Math2)),
+                                TRUE, FALSE
+                                )
+  
   progress$error <- ifelse(progress$attend_error == T |
                              progress$math_error == T | 
                              progress$science_error == T |
@@ -113,18 +122,19 @@ progress_import_script <- function(progress){
   progress$`Baseline_Attendance_Rate` <- NULL
   progress$Baseline_ELA <- NULL
   progress$Baseline_Math <- NULL
+  progress$Baseline_Math2 <- NULL
   progress$Baseline_Science <- NULL
   progress$Baseline_Suspensions <- NULL
 
   progress$Target_ELA <- NULL
   progress$`Target_Attendance_Rate` <- NULL
   progress$Target_Math <- NULL
+  progress$Target_Math2 <- NULL
   progress$Target_Suspensions <- NULL
   progress$Target_Science <- NULL
 
-  progress[,c(1:2, 4:6)] <- NULL
+  progress[,c(1:2, 4)] <- NULL
   progress$Student.ID <- as.character(progress$Student.ID)
-  progress$Enrollment.Status <- NULL
 
   return(progress)
   
